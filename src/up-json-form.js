391ad3ee66d415eb5be9507bcbedb6fa939cc131
Robert Ream <robert@stably.io>
2025-04-27 22:37:38 -0700

    flat field submission

--- src/up-json-form.js
+++ src/up-json-form.js
@@ -5,13 +5,28 @@ up.compiler('form[enctype="application/json"]', function(form) {
   form.addEventListener('submit', function(event) {
     event.preventDefault()
 
-    const jsonBody = {}
+    const formData = {}
+    const elements = form.elements
+
+    for (let element of elements) {
+      if (!element.name || element.disabled) continue
+
+      let value = element.value
+
+      if (element.type === 'number') {
+        value = (value === '') ? null : Number(value)
+      } else if (element.type === 'checkbox') {
+        value = element.checked
+      }
+
+      formData[element.name] = value
+    }
 
     up.submit(form, {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(jsonBody)
+      body: JSON.stringify(formData)
     })
   })
 })
